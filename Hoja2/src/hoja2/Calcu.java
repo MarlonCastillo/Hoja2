d3f00ee9c89ff5b88addbe450ed54831347e8139
MarlonCastillo <marlonjcastillo@hotmail.com>
2015-07-26 19:24:22 -0600

    Update Calcu.java

--- Hoja2/src/hoja2/Calcu.java
+++ Hoja2/src/hoja2/Calcu.java
@@ -1,3 +1,22 @@
-public class Calcu {
+public class Calcu implements Calculadora{
+    private String linea;
+    private int Resultado;
+    private Stack<String> pila = new StackArrayList<String>();
     
+    public void setString(String linea){
+        this.linea=linea;
+    }
+    public void meterVector(){
+        for (int a=linea.length()-1;a>=0;a--){
+            if (String.valueOf(linea.charAt(a)).equals(" ")){a=a;}
+            else {pila.push(String.valueOf(linea.charAt(a)));}
+        }
+    }
+    public boolean calcularVector(){
+        Resultado = Integer.parseInt(pila.pop()) + Integer.parseInt(pila.pop());
+        return true;
+    }
+    public int getResultado(){
+        return Resultado;
+    }
 }
