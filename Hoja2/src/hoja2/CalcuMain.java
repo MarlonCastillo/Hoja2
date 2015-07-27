8d1973e17fcfee7c1346130af5d93a2109a2dd38
MarlonCastillo <marlonjcastillo@hotmail.com>
2015-07-26 19:25:32 -0600

    Update CalcuMain.java

--- Hoja2/src/hoja2/CalcuMain.java
+++ Hoja2/src/hoja2/CalcuMain.java
@@ -9,5 +9,13 @@ public class Calcu {
     	System.out.println("Ingrese su operacion en sistema Postfix: ") ;
 			linea=lector.nextLine();
 			calcu.setString(linea);
+	/*
+	Calculadora miCalcu = new Calcu();
+        System.out.println("BIENVENIDO A NUESTRA CALCULADORA POSTFIX.\nRecorriendo archivo de texto, por favor espere a que se procesen los datos...");
+        miCalcu.setString("1 2 + 4 * 3 +");
+        miCalcu.meterVector();
+        miCalcu.calcularVector();
+        System.out.println(miCalcu.getResultado());
+	*/
     }
 }
