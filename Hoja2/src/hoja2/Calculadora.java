8979c35f1a11f1e6df60e6fc3716d6dca53e8d1f
sebascas <frnndsebatian@hotmail.com>
2015-07-26 18:16:45 -0600

    Interfaz

---
+++ Hoja2/src/hoja2/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja2;
+
+/**
+ *
+ * @author Sebastian
+ */
+public interface Calculadora {
+    
+    public void setString(String linea);
+    
+    public void meterVector ();
+    
+    public boolean calcularVector();
+   
+    
+    public int getResultado();
+    
+    
+}
