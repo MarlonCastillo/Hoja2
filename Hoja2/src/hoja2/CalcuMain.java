fbc31ddadf649a2a4931bb9b6bf03a600721dc35
majo0103 <majito0103@gmail.com>
2015-07-26 19:17:53 -0600

    CalcuMain.java

---
+++ Hoja2/src/hoja2/CalcuMain.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Calcu {
+    public static void main(String[] args) {
+    	Calculadora <Vector> calcu = new Calcu <Vector>;
+
+    	String linea;
+
+    	System.out.println("Ingrese su operacion en sistema Postfix: ") ;
+			linea=lector.nextLine();
+			calcu.setString(linea);
+    }
+}
