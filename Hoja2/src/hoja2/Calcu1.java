e8d6b1e3156585d6df8530409c43ff73e695f875
lim14373 <lim14373@uvg.edu.gt>
2015-07-29 16:00:11 -0600

    Modificaciones, sirve igual

--- Hoja2/src/hoja2/Calcu1.java
+++ Hoja2/src/hoja2/Calcu1.java
@@ -1,5 +1,6 @@
-public class Calcu implements Calculadora{
-    private String linea, resultado;
+public class Calcu implements Calculadora
+{
+    private String linea, resultado, operando;
     private int Resultado, num1, num2;
     private Stack<Integer> operandos = new StackVector<Integer>();
     private Stack<String> operadores = new StackVector<String>();
@@ -7,62 +8,55 @@ public class Calcu implements Calculadora{
     public void setString(String linea){
         this.linea=linea;
     }
-    public void meterVector(){
-        for (int a=linea.length()-1;a>=0;a--){
+	
+    public void meterVector()
+	{
+        for (int a=linea.length()-1;a>=0;a--)
+		{
             String b=String.valueOf(linea.charAt(a));
-            if (b.equals(" ")){ a=a; }
-            else if(b.equals("+") || b.equals("-") || b.equals("*") || b.equals("/")){ operadores.push(b); }
-            else {operandos.push(Integer.parseInt(b)); }
+            if (b.equals(" "))
+			{
+				a=a;
+			}else if(b.equals("+") || b.equals("-") || b.equals("*") || b.equals("/"))
+			{
+				operadores.push(b);
+			}else
+			{
+				operandos.push(Integer.parseInt(b));
+			}
         }
     }
-    public boolean calcularVector(){
-
-
-           for (int a=0;a<=operadores.size();a++){
-           if(operadores.peek().equals("+")){ 
-              operadores.pop();
-              num1= operandos.pop();
-              num2=operandos.pop(); 
-              Resultado= num1 + num2;
-              System.out.println(Resultado);
-              operandos.push(Resultado);
-            }
-
-           else if(operadores.peek().equals("-")){
-              operadores.pop();
-              int num1= operandos.pop();
-              int num2=operandos.pop(); 
-              Resultado= num1 - num2;
-              System.out.println(Resultado);
-              operandos.push(Resultado);
-            }
-           else if(operadores.peek().equals("/")){
-              operadores.pop();
-              int num1= operandos.pop();
-              int num2=operandos.pop(); 
-              Resultado= num1 / num2;
-              System.out.println(Resultado);
-              operandos.push(Resultado);
-            }
-           else if(operadores.peek().equals("*")){
-              operadores.pop();
-              int num1= operandos.pop();
-              int num2=operandos.pop(); 
-              Resultado= num1 * num2;
-              System.out.println(Resultado);
-              operandos.push(Resultado);
-             }
-        
-        }
-
-        
-        Resultado = operandos.pop();
-
-        
+	
+    public boolean calcularVector()
+	{
+		num1 = operandos.pop();
+		num2 = operandos.pop();
+		for (int a=0;a<operadores.size();a++)
+		{
+			operando = operadores.pop();
+			switch(operando)
+			{
+				case "+":
+					Resultado = num1 + num2;
+					break;
+				case "-":
+					Resultado = num1 - num2;
+					break;
+				case "/":
+					Resultado = num1 / num2;
+					break;
+				case "*":
+					Resultado = num1 * num2;
+					break;
+				default:
+					break;
+			}
+		}
         return true;
     }
 
-    public int getResultado(){
-        return Resultado;
+    public int getResultado()
+	{
+		return Resultado;
     }
 }
