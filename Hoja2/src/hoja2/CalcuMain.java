dcc73d73eaf870857f8c958036738b58ca0a3697
majo0103 <majito0103@gmail.com>
2015-07-29 07:56:06 -0600

    Update CalcuMain.java

--- Hoja2/src/hoja2/CalcuMain.java
+++ Hoja2/src/hoja2/CalcuMain.java
@@ -1,21 +1,42 @@
 import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class CalcuMain {
     public static void main(String[] args) {
 
+    try{
+        FileReader lector=new FileReader("texto.txt");
+        //El contenido de lector se guarda en un BufferedReader
+        BufferedReader contenido=new BufferedReader(lector);
     	String linea;
         Calculadora miCalcu = new Calcu();
         System.out.println("BIENVENIDO A NUESTRA CALCULADORA POSTFIX.\nRecorriendo archivo de texto, por favor espere a que se procesen los datos...");
-        Scanner lector= new Scanner(System.in);
 
-        linea=lector.nextLine();
+        //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
+        while((linea=contenido.readLine())!=null)
+        {
+            miCalcu.setString(linea);
+            miCalcu.meterVector();
+            miCalcu.calcularVector();
+            System.out.println("El resultado de la operacion es: "+miCalcu.getResultado());
+            //System.out.println(linea);
 
-        miCalcu.setString(linea);
-        miCalcu.meterVector();
-        miCalcu.calcularVector();
-        System.out.println("El resultado de la operacion es: "+miCalcu.getResultado());
+        }
+        }
+
+        //Si se causa un error al leer cae aqui
+        catch(Exception e)
+        {
+        System.out.println("Error al leer");
+        }
+        //linea=lector.nextLine();
+
+        
     }
+        
+
+        
+        
+
 }
-
-
-//"1 2 + 4 * 3 +"
