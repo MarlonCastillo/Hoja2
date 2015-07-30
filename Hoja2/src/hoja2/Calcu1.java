bc38e6f8216b4d81e72e50e9157b1c2c4ff87aef
lim14373 <lim14373@uvg.edu.gt>
2015-07-29 18:36:26 -0600

    Versión Final con programación defensiva.

--- Hoja2/src/hoja2/Calcu1.java
+++ Hoja2/src/hoja2/Calcu1.java
@@ -1,19 +1,75 @@
 public class Calcu implements Calculadora
 {
-    private String linea, resultado, operando;
-    private int Resultado, num1, num2;
+    private String linea, resultado, operador;
+    private int Resultado, num1, num2, rpivote;
     private Stack<Integer> operandos = new StackVector<Integer>();
     private Stack<String> operadores = new StackVector<String>();
+	private Stack<Integer> operandos2 = new StackVector<Integer>();
     
-    public void setString(String linea){
+    public void setString(String linea)
+	{
         this.linea=linea;
     }
 	
     public void meterVector()
 	{
-        for (int a=linea.length()-1;a>=0;a--)
+		Resultado = 0;
+		int d = operadores.size();
+		for(int b = operadores.size(); b>0; b--)
+		{
+			num1 = operandos2.pop();
+			if(b == d)
+			{
+				num2 = operandos2.pop();
+			}else
+			{
+				num2 = Resultado;
+			}
+			
+			operador = operadores.pop();
+			switch(operador)
+			{
+				case "+":
+					Resultado = num2 + num1;
+					break;
+				case "-":
+					Resultado = num2 - num1;
+					break;
+				case "/":
+					if (num1 == 0)
+					{
+						System.out.println("Division en 0 es indeterminado.");
+					}else
+					{
+						Resultado = num2 / num1;
+					}
+					break;
+				case "*":
+					Resultado = num2 * num1;
+					break;
+				default:
+					break;
+			}
+		}
+    }
+	
+    public boolean calcularVector()
+	{
+		boolean devolver = false;
+		int c = 0;
+		int d = 0;
+		for (int a = operandos2.size(); a>0; a--)
+		{
+			operandos2.pop();
+		}
+		for (int a = operadores.size(); a>0; a--)
+		{
+			operadores.pop();
+		}
+		for (int a=linea.length()-1;a>=0;a--)
 		{
             String b=String.valueOf(linea.charAt(a));
+			
             if (b.equals(" "))
 			{
 				a=a;
@@ -22,37 +78,32 @@ public class Calcu implements Calculadora
 				operadores.push(b);
 			}else
 			{
-				operandos.push(Integer.parseInt(b));
+				try{
+					operandos.push(Integer.parseInt(b));
+				}catch(Exception e)
+				{
+					devolver = true;
+				}
 			}
         }
-    }
-	
-    public boolean calcularVector()
-	{
-		num1 = operandos.pop();
-		num2 = operandos.pop();
-		for (int a=0;a<operadores.size();a++)
+		for (int a=operandos.size();a>0;a--)
 		{
-			operando = operadores.pop();
-			switch(operando)
-			{
-				case "+":
-					Resultado = num1 + num2;
-					break;
-				case "-":
-					Resultado = num1 - num2;
-					break;
-				case "/":
-					Resultado = num1 / num2;
-					break;
-				case "*":
-					Resultado = num1 * num2;
-					break;
-				default:
-					break;
-			}
+			operandos2.push(operandos.pop());
 		}
-        return true;
+		c = operadores.size() + 1;
+		d = operandos2.size();
+		
+		if (c == d)
+		{
+			devolver = false;
+		}else{
+			devolver = true;
+		}
+		if(devolver)
+		{
+			System.out.println("Ha ingresado mal el formato POSTFIX, pruebe otra vez:");
+		}
+		return devolver;
     }
 
     public int getResultado()
