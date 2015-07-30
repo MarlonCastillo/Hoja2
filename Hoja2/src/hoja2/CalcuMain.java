62d912dbe48cab24bbfc8096c5ea5c527578174f
lim14373 <lim14373@uvg.edu.gt>
2015-07-29 22:03:48 -0600

    Última versión

--- Hoja2/src/hoja2/CalcuMain.java
+++ Hoja2/src/hoja2/CalcuMain.java
@@ -1,42 +1,32 @@
-import java.util.Scanner;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class CalcuMain {
     public static void main(String[] args) {
-
-    try{
-        FileReader lector=new FileReader("texto.txt");
-        //El contenido de lector se guarda en un BufferedReader
-        BufferedReader contenido=new BufferedReader(lector);
-    	String linea;
-        Calculadora miCalcu = new Calcu();
         System.out.println("BIENVENIDO A NUESTRA CALCULADORA POSTFIX.\nRecorriendo archivo de texto, por favor espere a que se procesen los datos...");
-
-        //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
-        while((linea=contenido.readLine())!=null)
-        {
-            miCalcu.setString(linea);
-            miCalcu.meterVector();
-            miCalcu.calcularVector();
-            System.out.println("El resultado de la operacion es: "+miCalcu.getResultado());
-            //System.out.println(linea);
-
+		try{
+            FileReader lector=new FileReader("texto.txt");
+            //El contenido de lector se guarda en un BufferedReader
+            BufferedReader contenido=new BufferedReader(lector);
+            //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
+            String linea;
+        	Calculadora miCalcu = new Calcu();
+            //System.out.println(linea=contenido.readLine());
+            while((linea=contenido.readLine())!=null) {
+            	System.out.println(linea);
+            	miCalcu.setString(linea);
+            	miCalcu.meterVector();
+            	if(miCalcu.calcularVector()){
+                	System.out.println("El resultado de la operacion es: "+miCalcu.getResultado());
+            	}
+            	else {System.out.println("La operacion es incorrecta y no se puede realizar.");}
+           	}
         }
-        }
-
         //Si se causa un error al leer cae aqui
-        catch(Exception e)
-        {
-        System.out.println("Error al leer");
+        catch(Exception e) {
+            System.out.println("Error al leer el archivo.");
         }
-        //linea=lector.nextLine();
-
-        
-    }
-        
-
-        
-        
-
+	}
 }
